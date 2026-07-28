09fdd0a0d1e4691864108949944ec0f888c67ee1
viarol200-byte <viarol200@gmail.com>
2026-07-28 18:44:48 +0300

    Update lua.lua

--- lua.lua
+++ lua.lua
@@ -1,4 +1,4 @@
--- Revenge Hub | Custom GUI by VRT PIDOR DEV
+-- Кастомный интерфейс от VRT PIDOR DEV
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local CoreGui = game:GetService("CoreGui")
